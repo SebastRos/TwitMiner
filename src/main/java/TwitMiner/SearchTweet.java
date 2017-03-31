f4f149efbdabb590bf58920dc724672f9dd352ad
Sébastien Rospars <sebastien.rospars@gmail.com>
2017-03-31 12:12:09 +0200

    Ajout de la recherche

--- src/main/java/TwitMiner/SearchTweet.java
+++ src/main/java/TwitMiner/SearchTweet.java
@@ -4,24 +4,43 @@ import twitter4j.*;
 
 import twitter4j.conf.ConfigurationBuilder;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class SearchTweet {
 
+    FileOutputStream fos= null;
+
+
     public static void main(String args[]) throws Exception {
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true)
-                .setOAuthConsumerKey("l6H0HBRCkTxWAjhRsfk97bWdk")
-                .setOAuthConsumerSecret("mvoAw2MWXaEfww565llBStV5cofzJrUbJCQMjt4RCXeXoS4MHg")
-                .setOAuthAccessToken("833975933606453250-4tpuplmCfo9fw2ma4aq3ypq2FWWwOAc")
-                .setOAuthAccessTokenSecret("VzwWyodmL0Tgn21CGtEAN1baqyKsHD3boHipDTN7coEIU");
-        TwitterFactory tf = new TwitterFactory(cb.build());
-        Twitter twitter = tf.getInstance();
+        try {
+            new FileOutputStream(new File("/Users/seb/Desktop/TwitMiner/data.csv"));
+            Query query = new Query("#TPMPLaGrandeRassrah2");
 
-        ResponseList<twitter4j.Status> statuses = twitter.getHomeTimeline();
-        System.out.println("Showing foot timeline.");
-        for (twitter4j.Status status : statuses) {
-            System.out.println(status.getCreatedAt()+" - "+status.getUser().getName() +" - "+ status.getUser().getLocation() +" : " +
-                    status.getText());
+            ConfigurationBuilder cb = new ConfigurationBuilder();
+            cb.setDebugEnabled(true)
+                    .setOAuthConsumerKey("l6H0HBRCkTxWAjhRsfk97bWdk")
+                    .setOAuthConsumerSecret("mvoAw2MWXaEfww565llBStV5cofzJrUbJCQMjt4RCXeXoS4MHg")
+                    .setOAuthAccessToken("833975933606453250-4tpuplmCfo9fw2ma4aq3ypq2FWWwOAc")
+                    .setOAuthAccessTokenSecret("VzwWyodmL0Tgn21CGtEAN1baqyKsHD3boHipDTN7coEIU");
 
+            TwitterFactory tf = new TwitterFactory(cb.build());
+            Twitter twitter = tf.getInstance();
+
+            QueryResult soumsoum=twitter.search(query);
+
+            System.out.println("Showing foot timeline.");
+
+            for (twitter4j.Status status : soumsoum.getTweets()) {
+                System.out.println(status.getCreatedAt() + " - " + status.getUser().getName() + " - " + status.getUser().getLocation() + " : " +
+                        status.getText());
+
+
+            }
+
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
 
         }
     }
