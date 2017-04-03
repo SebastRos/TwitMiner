7988b06adbebd580d576e325b02610ff1e5c115c
Sébastien Rospars <sebastien.rospars@gmail.com>
2017-04-03 16:48:09 +0200

    Map remplie

--- src/main/java/TwitMiner/CsvToTrans.java
+++ src/main/java/TwitMiner/CsvToTrans.java
@@ -1,13 +1,49 @@
 package TwitMiner;
 
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.*;
+import java.util.Map;
 
 public class CsvToTrans {
 
     private static BufferedWriter bw = null;
     private static BufferedReader rw = null;
+    private static Map<Integer, String> stockWord = null;
+    private static int key=1;
+    private static String ligne;
 
 
+    public CsvToTrans() {
+        try{
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("data.trans")));
+            rw = new BufferedReader(new InputStreamReader(new FileInputStream("data.csv")));
+
+            while ((ligne = rw.readLine())!=null){
+                String[] parts = ligne.split(";");
+                for (String word : parts){
+                    boolean isExisting = false;
+
+                    for (final Map.Entry<Integer,String> entry : stockWord.entrySet()){
+                        if (word.equals(entry.getValue()))
+                            isExisting =true;
+                    }
+                    if (!isExisting){
+                        stockWord.put(key, word);
+                        ++key;
+                    }
+                }
+            }
+
+
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        CsvToTrans test = new CsvToTrans();
+    }
 }
