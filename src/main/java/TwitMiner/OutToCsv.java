dd5db893a6f5e191a531d4498ce28bc859692223
Sébastien Rospars <sebastien.rospars@gmail.com>
2017-04-07 14:51:19 +0200

    Constructeur en cours

--- src/main/java/TwitMiner/OutToCsv.java
+++ src/main/java/TwitMiner/OutToCsv.java
@@ -1,17 +1,33 @@
 package TwitMiner;
 
 import java.io.*;
+import java.util.Map;
 
 public class OutToCsv {
 
     private static BufferedWriter bw = null;
     private static BufferedReader rw = null;
+    private static BufferedReader rw2 = null;
+    private String ligne;
+
 
     public OutToCsv() {
         try{
-            rw = new BufferedReader(new InputStreamReader(new FileInputStream("data.csv")));
-            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("map.csv")));
+            rw = new BufferedReader(new InputStreamReader(new FileInputStream("data.out")));
+            rw2 = new BufferedReader(new InputStreamReader(new FileInputStream("map.csv")));
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("result.csv")));
+
+            while ((ligne = rw.readLine())!=null){
+                String[] parts = ligne.split(" ");
+                
+            }
+
+
+
+
         } catch (FileNotFoundException e){
+            e.printStackTrace();
+        } catch (IOException e){
             e.printStackTrace();
         }
 
